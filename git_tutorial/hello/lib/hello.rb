0a1841ba327b95067a297575fefd7aa2d89e319f
aswi patel <apate187@uncc.edu>
2019-09-08 02:53:36 +0000

    Merged master fixed conflict.

--- git_tutorial/hello/lib/hello.rb
+++ git_tutorial/hello/lib/hello.rb
@@ -1,4 +1,7 @@
-puts "What's your name"
-my_name = Aswi.Patel
+require 'greeter'
 
-puts "Hello, #{my_name}!"
+puts "What's your name"
+my_name = gets.strip
+
+greeter = Greeter.new(my_name)
+puts greeter.greet
