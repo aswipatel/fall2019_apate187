27d0955f8d7560fc67757be2c9a29ea0274baaf0
aswi patel <apate187@uncc.edu>
2019-09-08 02:51:56 +0000

    Made interactive

--- git_tutorial/hello/lib/hello.rb
+++ git_tutorial/hello/lib/hello.rb
@@ -1,7 +1,4 @@
-require 'greeter'
+puts "What's your name"
+my_name = Aswi.Patel
 
-# Default is World
-name = ARGV.first || "World"
-
-greeter = Greeter.new(name)
-puts greeter.greet
+puts "Hello, #{my_name}!"
