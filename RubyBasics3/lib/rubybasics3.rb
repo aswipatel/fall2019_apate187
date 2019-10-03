5573de4acf278a5b67c3d1f88d6b2f06bae803d3
aswi patel <apate187@uncc.edu>
2019-10-03 17:14:41 +0000

    Ruby 00 Basics -Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -2,4 +2,10 @@
 
 class BookInStock
 # YOUR CODE HERE
+    attr_accessor :isbn, :price
+    def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+   end  
 end
