456ebd6a707b8d7cfcf4c47a501bb1185673667a
aswi patel <apate187@uncc.edu>
2019-11-14 17:23:00 +0000

    Adding user functionality

--- LABS/blog/app/controllers/application_controller.rb
+++ LABS/blog/app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
-end
+    helper_method :current_user
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find(session[:user_id]) 
+        else
+             @current_user = nil
+        end 
+    end
+end 
+    def create
+        @article =Article.new(article_params)
+        @article.user = current_user
+        
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end 
+    end
--- LABS/blog/config/routes.rb
+++ LABS/blog/config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy'
   
-  resources :articles do
+  resources :sessions, only: [:new, :create, :destroy]
+    get 'signup', to: 'users#new', as: 'signup'
+    get 'login', to: 'sessions#new', as: 'login'
+    get 'logout', to: 'sessions#destroy', as: 'logout'
+    
+    
+  resources :article do
     resources :comments
   end
   
   root 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
